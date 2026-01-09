037efbe30cb685bd83a10b62a5dbcc25cf16183f
Panshulp <panshul_purusharthy27@milton.edu>
2026-01-08 20:50:21 -0500

    Made a380 image

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -38,11 +38,11 @@ public class BasicGameApp implements Runnable {
    public JPanel panel;
    
 	public BufferStrategy bufferStrategy;
-	public Image astroPic;
+	public Image a380Pic;
 
    //Declare the objects used in the program
    //These are things that are made up of more than one variable type
-	private Astronaut astro;
+	private plane aircraft;
 
 
    // Main method definition
@@ -63,8 +63,8 @@ public class BasicGameApp implements Runnable {
        
       //variable and objects
       //create (construct) the objects needed for the game and load up 
-		astroPic = Toolkit.getDefaultToolkit().getImage("astronaut.png"); //load the picture
-		astro = new Astronaut(10,100);
+		a380Pic = Toolkit.getDefaultToolkit().getImage("aqgame380.png"); //load the picture
+		aircraft = new plane(10,100);
 
 
 	}// BasicGameApp()
@@ -92,7 +92,7 @@ public class BasicGameApp implements Runnable {
 	public void moveThings()
 	{
       //calls the move( ) code in the objects
-		astro.move();
+		aircraft.move();
 
 	}
 	
@@ -142,8 +142,8 @@ public class BasicGameApp implements Runnable {
 		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 
-      //draw the image of the astronaut
-		g.drawImage(astroPic, astro.xpos, astro.ypos, astro.width, astro.height, null);
+      //draw the image of the aircraft
+		g.drawImage(a380Pic, aircraft.xpos, aircraft.ypos, aircraft.width, aircraft.height, null);
 
 		g.dispose();
 
